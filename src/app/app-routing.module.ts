faf93ae57e3e7ee9022f5ccf3f54f259fe2dbbee
gdserafini <gdinizserafini@gmail.com>
2024-03-29 16:05:10 -0300

    initial config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { CustomerComponent } from './customer/customer.component';
+import { CarComponent } from './car/car.component';
+import { RentComponent } from './rent/rent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "layout", component: LayoutComponent, 
+  children: [
+    {path: "home", component: HomeComponent},
+    {path: "signup", component: SignupComponent},
+    {path: "login", component: LoginComponent},
+    {path: "rent", component: RentComponent}
+  ]},
+  {path: "user", component: UserComponent},
+  {path: "customer", component: CustomerComponent},
+  {path: "car", component: CarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { CarComponent } from './car/car.component';
+import { RentComponent } from './rent/rent.component';
+import { UserComponent } from './user/user.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    CarComponent,
+    RentComponent,
+    UserComponent,
+    LayoutComponent,
+    HomeComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
