c5752d5fc1aaac9b196c467af81577e9e04175df
gdserafini <gdinizserafini@gmail.com>
2024-06-09 21:56:25 -0300

    users crud -> update (complete)

--- src/app/users/read-user/read-user.component.ts
+++ src/app/users/read-user/read-user.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { UserServiceService } from '../service/user-service.service';
+import { Router } from '@angular/router';
+import { map } from 'rxjs';
+import { UserModel } from '../model/user-model';
 
 @Component({
   selector: 'app-read-user',
@@ -6,5 +10,33 @@ import { Component } from '@angular/core';
   styleUrl: './read-user.component.css'
 })
 export class ReadUserComponent {
+  public usersList: any;
 
+  constructor(
+    private service: UserServiceService,
+    private router: Router
+  ){}
+
+  ngOnInit(): void { this.listUsers(); }
+
+  listUsers(){
+    return this.service.readAllUsers()
+      .pipe(
+        map(changes => {
+          return changes.map(u => ({ key: u.key, 
+            ...u.payload.val() as UserModel}));
+        })
+      )
+      .subscribe(users => {
+        this.usersList = users;
+      });
+  }
+
+  updateUser(code: string){ 
+    this.router.navigate(['/update-user/'+code]);
+  }
+
+  deleteUser(code: string){
+    return this.service.delete(code);
+  }
 }
--- src/app/users/update-user/update-user.component.ts
+++ src/app/users/update-user/update-user.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { UserServiceService } from '../service/user-service.service';
+import { ActivatedRoute } from '@angular/router';
+import { UserModel } from '../model/user-model';
 
 @Component({
   selector: 'app-update-user',
@@ -6,5 +10,36 @@ import { Component } from '@angular/core';
   styleUrl: './update-user.component.css'
 })
 export class UpdateUserComponent {
+  name: FormControl = new FormControl('');
+  email: FormControl = new FormControl('');
+  cpf: FormControl = new FormControl(
+    '', [Validators.pattern('[0-9]{11}')]);
+  phone: FormControl = new FormControl(
+    '', [Validators.pattern('[0-9]{11}')]);
+  password: FormControl = new FormControl('');
 
+  constructor(
+    private service: UserServiceService,
+    private router: ActivatedRoute
+  ){}
+
+  ngOnInit(): void {}
+
+  update(): void {
+    if(this.cpf.invalid || this.phone.invalid){
+        console.log('Dados inválidos');
+        return;
+    }
+    const key = this.router.snapshot.params['code'];
+    const dbUser = this.service.getUser(key);
+    dbUser.subscribe((u: any) => {
+      const user: UserModel = new UserModel();
+      user.name = this.name.value ? this.name.value : u.name;
+      user.email = this.email.value ? this.email.value : u.email;
+      user.cpf = this.cpf.value ? this.cpf.value : u.cpf;
+      user.phone = this.phone.value ? this.phone.value : u.phone;
+      user.password = this.password.value ? this.password.value : u.password;
+      this.service.update(key, user);
+    });
+  }
 }
